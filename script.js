c84f9f5673c2cf1af99a111b7f57e3eaee66fffe
mauricio119 <mauricio@comebien.mx>
2026-01-19 16:28:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const meses = [
-  "ENERO", "FEBRERO", "MARZO", "ABRIL",
-  "MAYO", "JUNIO", "JULIO", "AGOSTO",
-  "SEPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"
+  "ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO",
+  "JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"
 ];
 
 function updateDate() {
   const now = new Date();
-  const day = now.getDate();
-  const month = meses[now.getMonth()];
-
-  document.querySelector("#day .top").textContent = day;
-  document.querySelector("#day .bottom").textContent = day;
-
-  document.querySelector("#month .top").textContent = month;
-  document.querySelector("#month .bottom").textContent = month;
+  document.querySelector("#day .display").textContent = now.getDate();
+  document.querySelector("#month .display").textContent = meses[now.getMonth()];
 }
 
 updateDate();
-setInterval(updateDate, 60 * 1000);
+setInterval(updateDate, 60000);
